673f49bc19f2ff465298d5facb6330af8ad0599b
discocat <me@runderscore.com>
2026-06-23 05:03:56 +0500

    send win packet to winner

--- src/server/models/match.py
+++ src/server/models/match.py
@@ -410,7 +410,7 @@ class MatchManager:
 
                     winner = next((pl for pl in match._players.values() if pl.player_id != player.player_id), None)
                     if winner:
-                        won = Packets.lost_match()
+                        won = Packets.won_match()
                         await io_handler.enqueue_single_out(Packets.envelope(won), winner.addr, True, won.msg_id)
 
                         await self._notify_api(winner=winner, loser=player)
